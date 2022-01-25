3cc16b9cf85030bd31638035def366a625d11182
czamora7 <czamora4@ewu.edu>
2022-01-24 16:21:33 -0800

    added nullability and property in logfactory

--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -1,7 +1,23 @@
-﻿namespace Logger
+﻿using System;
+
+namespace Logger
 {
     public class LogFactory
     {
+
+        public string _classname;
+
+        public string Classname
+        {
+            get => _classname;
+            
+            set
+            {
+                if (value is null) throw new ArgumentNullException(nameof(value));
+                _classname = value;
+            }
+        }
+
         public BaseLogger CreateLogger(string className)
         {
             
