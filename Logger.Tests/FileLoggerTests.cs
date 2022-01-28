e48f3dfa11be5699835b17b8d99a6d8131b6fc22
czamora7 <czamora4@ewu.edu>
2022-01-27 21:34:21 -0800

    mended fileloggertests

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Logger.Tests
 {
     [TestClass]
     public class FileLoggerTests
     {
-        
+        [TestMethod]
+        public void TestLogHasCorrectOutput()
+        {
+            FileLogger logger = new();
+            logger.Log(nameof(FileLogger), LogLevel.Error, "testing", "");
+            Assert.AreEqual(Console.ReadLine(), $"{DateTime.Now} {nameof(FileLogger)} {LogLevel.Error} testing");
+        }
     }
 }
