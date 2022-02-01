36c0c29637f7da9b9f6f267f24ac30ab26d64555
czamora7 <czamora4@ewu.edu>
2022-01-31 22:14:23 -0800

    completed jester class

--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -1,20 +1,20 @@
 ﻿namespace CanHazFunny;
 public class Jester
 {
-    private Comedian _Comedian;
-    private JokeService _JokeService;
-    protected Comedian? Comedian 
+    private Comedian? _Comedian;
+    private JokeService? _JokeService;
+    protected Comedian Comedian 
     {
-        get => _Comedian;
+        get => _Comedian!;
         set 
         { 
             if (value is null) throw new ArgumentNullException(nameof(value));
             _Comedian = value;
         } 
     }
-    protected JokeService? JokeService
+    protected JokeService JokeService
     {
-        get => _JokeService;
+        get => _JokeService!;
         set
         {
             if (value is null) throw new ArgumentNullException(nameof(value));
@@ -22,13 +22,27 @@ public class Jester
         }
     }
 
-    public void TellJoke(Comedian? comedian, JokeService? jokeService)
+    public void TellJoke(Comedian comedian, JokeService jokeService)
     {
         //set the dependencies as the parameters
+        Comedian = comedian;
+        JokeService = jokeService;
+        
         //retrieve the joke from the jokeservice
-        //check to see if the joke is a chuck norris joke
-        //if it is get another one
+        string joke = this.JokeService.GetJoke();
+        
+        //check to see if the joke is a chuck norris joke, if it is get another one
+#pragma warning disable CA1307 //this warning asked to specify the string comparison type, which 
+                                //required specific enums be set in place not necessarily relevant
+                                //in this string comparison
+        while(joke.Contains("Chuck Norris"))
+#pragma warning restore CA1307 
+        {
+            joke = this.JokeService.GetJoke();
+        }
+
         //if not display the joke and exit
+        this.Comedian.DisplayJoke(joke);
     }
 
 }
--- CanHazFunny/CanHazFunny/JokeService.cs
+++ CanHazFunny/CanHazFunny/JokeService.cs
@@ -8,7 +8,9 @@ public class JokeService : IJokeable
 
     public string GetJoke()
     {
+#pragma warning disable CA2234 // This required passing the system uri objects instead of strings
         string joke = HttpClient.GetStringAsync("https://geek-jokes.sameerkumar.website/api").Result;
+#pragma warning restore CA2234 
         return joke;
     }
 }
